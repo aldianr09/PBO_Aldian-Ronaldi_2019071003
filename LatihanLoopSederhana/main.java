db7a784e08463c7a0e7a0774c339342a0386ba9d
aldianr09 <71371512+aldianr09@users.noreply.github.com>
2020-10-03 14:53:35 +0700

    Add files via upload

---
+++ LatihanLoopSederhana/main.java
@@ -0,0 +1,27 @@
+package LatihanLoopSederhana;
+
+import java.util.*;
+
+public class main {
+    public static void main (String[] args){
+
+        // program untuk menjumlahkan angka dengan rentang
+
+        int nilaiAwal, nilaiAkhir, total;
+
+        Scanner inputUser = new Scanner(System.in);
+        System.out.print("masukan nilai awal = ");
+        nilaiAwal = inputUser.nextInt();
+        System.out.print("masukan nilai akhir = ");
+        nilaiAkhir = inputUser.nextInt();
+
+        total = 0;
+
+        while(nilaiAwal <= nilaiAkhir){
+            total = total + nilaiAwal;
+            System.out.println("ditambah " + nilaiAwal + " menjadi " + total);
+            nilaiAwal++;
+        }
+        // tugas -> for loop, do while...
+    }
+}
