ecfc3c250ad3189846edbbbcf9788bf2776385c3
aldianr09 <71371512+aldianr09@users.noreply.github.com>
2020-10-12 16:04:25 +0700

    Add files via upload

---
+++ ContohForEach.java
@@ -0,0 +1,10 @@
+package com.pbo;
+
+public class ContohForEach {
+    public static void main(String[] args) {
+        String[] cars = {"volvo", "BMW", "ford"};
+        for (String i : cars) {
+            System.out.print(i + " ");
+        }
+    }
+}
